f08eafe9bf76291f84c68546aa0ff1ff6aa9784e
Ryan Santiago <ryan@ryans-mbp-16.attlocal.net>
2022-10-01 03:01:32 -0400

    Begin building out the home page frontend UI.

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div className='hero-banner-container'>
+      <div>
+        <p className='bose-qc'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src='' alt='headphones' className='hero-banner-image' />
+        <div>
+          <Link href={'/product/id'}>
+            <button type='button'>BUTTON TEXT</button>
+          </Link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
