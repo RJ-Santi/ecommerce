9efe41c968249421be119b49274779b8c12eedaf
Ryan Santiago <ryan@ryans-mbp-16.attlocal.net>
2022-10-04 20:31:42 -0400

    Finish UI template.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai'
+import { Product } from '../../components'
 
 import {client, urlFor} from '../../lib/client'
 
 const ProductDetails = ({ product, products }) => {
     const { image, details, name, price } = product
 
+const [index, setIndex] = useState(0)
+
   return (
     <div>
         <div className='product-detail-container'>
             <div>
                 <div className='image-container'>
-                    <img src={urlFor(image && image[0])}  />
+                    <img src={urlFor(image && image[index])} className='product-detail-image'  />
                 </div>
-                {/* <div className='small-images-container'>
+                <div className='small-images-container'>
                     {image?.map((item, i) => (
-                        <img src={urlFor(item)} className='' onMouseEnter='' />
+                        <img src={urlFor(item)} className={i === index ? 'small-image selected-image' : 'small-image'} onMouseEnter={() => setIndex(i)} />
                     ))}
-                </div> */}
+                </div>
             </div>
-            <div className='product-details-desc'>
+            <div className='product-detail-desc'>
                 <h1>{name}</h1>
                 <div className='reviews'>
                     <div>
@@ -47,6 +50,20 @@ const ProductDetails = ({ product, products }) => {
                             <AiOutlinePlus />
                         </span>
                     </p>
+                </div>
+                <div className='buttons'>
+                    <button type='button' className='add-to-cart' onclick=''>Add to Cart</button>
+                    <button type='button' className='buy-now' onclick=''>Buy Now</button>
+                </div>
+            </div>
+        </div>
+        <div className='maylike-products-wrapper'>
+            <h2>You May Also Like</h2>
+            <div className='marquee'>
+                <div className='maylike-products-container track'>
+                    {products.map((item) => (
+                        <Product key={item._id} product={item} />
+                    ))}
                 </div>
             </div>
         </div>
@@ -81,9 +98,6 @@ export const getStaticProps = async ({ params: {slug} }) => {
   
   const product = await client.fetch(query)
   const products = await client.fetch(productsQuery)
-
-  console.log(product);
-
 
   return {
     props: { product, products}
