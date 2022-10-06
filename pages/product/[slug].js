ed9a0b5fc8420724d76fb815b0320d16f7c71e12
Ryan Santiago <ryan@ryans-mbp-16.attlocal.net>
2022-10-06 05:18:31 -0400

    Complete context logic and begin development of the cart.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,13 +1,13 @@
 import React, {useState} from 'react'
 import {AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar} from 'react-icons/ai'
 import { Product } from '../../components'
-
 import {client, urlFor} from '../../lib/client'
+import { useStateContext } from '../../context/StateContext'
 
 const ProductDetails = ({ product, products }) => {
     const { image, details, name, price } = product
-
-const [index, setIndex] = useState(0)
+    const [index, setIndex] = useState(0)
+    const {decQty, incQty, qty, onAdd} = useStateContext()
 
   return (
     <div>
@@ -40,20 +40,20 @@ const [index, setIndex] = useState(0)
                 <div className='quantity'>
                     <h3>Quantity: </h3>
                     <p className='quantity-desc'>
-                        <span className='minus' onClick=''>
+                        <span className='minus' onClick={decQty}>
                             <AiOutlineMinus />
                         </span>
-                        <span className='num' onClick=''>
-                            0
+                        <span className='num'>
+                            {qty}
                         </span>
-                        <span className='plus' onClick=''>
+                        <span className='plus' onClick={incQty}>
                             <AiOutlinePlus />
                         </span>
                     </p>
                 </div>
                 <div className='buttons'>
-                    <button type='button' className='add-to-cart' onclick=''>Add to Cart</button>
-                    <button type='button' className='buy-now' onclick=''>Buy Now</button>
+                    <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>Add to Cart</button>
+                    <button type='button' className='buy-now' onClick=''>Buy Now</button>
                 </div>
             </div>
         </div>
