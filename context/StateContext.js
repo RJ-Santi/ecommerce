f8078fbbcde80b3126af7a19bf9b8f89ae2bb749
Ryan Santiago <ryan@ryans-mbp-16.attlocal.net>
2022-10-08 02:54:58 -0400

    Complete cart logic.

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState(0)
     const [qty, setQty] = useState(1)
 
+    let foundProduct
+    let index
+
     const incQty = () => {
     setQty((prevQty) => prevQty + 1)
   }
@@ -47,6 +50,33 @@ export const StateContext = ({ children }) => {
     toast.success(`${qty} ${product.name} added to the cart.`)
   }
 
+  const onRemove = (product) => {
+    foundProduct = cartItems.find((item) => item._id === product._id)
+    const newCartItems = cartItems.filter((item) => item._id !== product._id)
+
+    setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity)
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - foundProduct.quantity)
+    setCartItems(newCartItems)
+  }
+
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id)
+    index = cartItems.findIndex((product) => product._id === id)
+    const newCartItems = cartItems.filter((item) => item._id !== id)
+
+    if(value === 'inc') {
+        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
+    } else if(value === 'dec') {
+        if(foundProduct.quantity > 1) {  
+            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }])
+            setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
+        }  
+    }
+  }
+
     return (
         <Context.Provider value={{
             showCart,
@@ -58,6 +88,8 @@ export const StateContext = ({ children }) => {
             decQty,
             onAdd,
             setShowCart,
+            toggleCartItemQuantity,
+            onRemove
         }}>
             {children}
         </Context.Provider>
