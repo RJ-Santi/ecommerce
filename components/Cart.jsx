2f868be33698783c1998edcd9aeb51a7d18840d3
RJ-Santi <cr7rs7@gmail.com>
2022-10-12 03:31:29 -0400

    Fix issue with cart reordering on update.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -7,7 +7,7 @@ import {
   AiOutlineShopping,
 } from 'react-icons/ai'
 import { TiDeleteOutline } from 'react-icons/ti'
-import toast, { Toast } from 'react-hot-toast'
+import toast from 'react-hot-toast'
 
 import { useStateContext } from '../context/StateContext'
 import { urlFor } from '../lib/client'
--- context/StateContext.js
+++ context/StateContext.js
@@ -4,14 +4,29 @@ import { toast } from "react-hot-toast";
 const Context = createContext()
 
 export const StateContext = ({ children }) => {
+    const initialCartState = []
+
     const [showCart, setShowCart] = useState(false)
-    const [cartItems, setCartItems] = useState([])
+    const [cartItems, setCartItems] = useState(initialCartState)
     const [totalPrice, setTotalPrice] = useState(0)
     const [totalQuantities, setTotalQuantities] = useState(0)
     const [qty, setQty] = useState(1)
 
     let foundProduct
     let index
+
+    // useEffect(() => {
+    //   const cartData = JSON.parse(localStorage.getItem('cartItems'))
+    //   if(cartData) {
+    //     setCartItems(cartData)
+    //   }
+    // }, [])
+
+    // useEffect(() => {
+    //   if(cartItems !== initialCartState) {
+    //     localStorage.setItem('cartItems', JSON.stringify(cartItems))
+    //   }
+    // }, [cartItems])
 
     const incQty = () => {
     setQty((prevQty) => prevQty + 1)
@@ -65,12 +80,12 @@ export const StateContext = ({ children }) => {
     const newCartItems = cartItems.filter((item) => item._id !== id)
 
     if(value === 'inc') {
-        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
+        setCartItems([...newCartItems.slice(0, index), { ...foundProduct, quantity: foundProduct.quantity + 1 }, ...newCartItems.slice(index)])
         setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
         setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
     } else if(value === 'dec') {
         if(foundProduct.quantity > 1) {  
-            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }])
+            setCartItems([...newCartItems.slice(0, index), { ...foundProduct, quantity: foundProduct.quantity - 1 }, ...newCartItems.slice(index)])
             setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
             setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1)
         }  
